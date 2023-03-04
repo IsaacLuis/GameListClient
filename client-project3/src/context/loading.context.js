1d516cbe22a47cfc069f042bb0aca022d95cd0ed
Luis Isaac <luiscubiche@gmail.com>
2023-03-03 23:20:25 -0500

    api resolve for now

--- client-project3/src/context/loading.context.js
+++ client-project3/src/context/loading.context.js
@@ -2,6 +2,7 @@ import { useState, createContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { get, post } from "../services/authService";
 import axios from "axios";
+import Games from "../pages/Games";
 
 const LoadingContext = createContext();
 
@@ -10,7 +11,7 @@ const LoadingProvider = ({ children }) => {
     const [isLoading, setIsLoading] = useState(false);
     const [user, setUser] = useState(null);
     const [message, setMessage] = useState('');
-
+    const [ games, setGames ] = useState(null);
 
     const setTimedMessage = (newMessage) => {
       setMessage(newMessage);
@@ -19,39 +20,52 @@ const LoadingProvider = ({ children }) => {
       }, 4000)
     }
 
+// 43a14242dd124f1fb5e0bb64b4a70da0
+// games?key=<key from RAWG>
+const getGames = async () => {
+  
+  if (!games) {
 
-
-
-const fetchQuotes = async () => {
-	try {
-        console.log("Calling API");
-		const res = await axios.get(
-			`https://famous-quotes4.p.rapidapi.com/random`,
+  
+	
+    console.log("Calling API"); 
+		await axios.get(
+			`https://rawg-video-games-database.p.rapidapi.com/games?key=43a14242dd124f1fb5e0bb64b4a70da0`,
 			{
 				headers: {
-					'x-rapidapi-host': 'https://rawg-video-games-database.p.rapidapi.com/games',
-					'x-rapidapi-key': '43a14242dd124f1fb5e0bb64b4a70da0'
+          'X-RapidAPI-Key': '3ab9eedeeemsh37ec609bf36b9b6p1b04b2jsnc001e82699f7',
+          'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
 				},
-				params: {category: 'all', count: '10'}
+				params: {page_size: '5'}
 			}
 		)
-        .then(()=>{
-               
-                console.log('res',  res);
+        .then((response)=>{
+               setGames(response.data.results)
+                console.log('res',  response.data.results);
         })
-	} catch (err) {
-		console.log(err);
-	}
+        .catch((err) => {
+          console.log(err)
+        })
+
+
 };
 
-    
+}
 
 
 
+// {condition && <ConditionalComponent />}
+//{fetchQuotes && <Games data={{fetchQuotes}} />}
     return (
-        <LoadingContext.Provider value={{ isLoading, fetchQuotes,message, setUser, user, setIsLoading, setMessage, setTimedMessage,}}>
+      <div>
+        <LoadingContext.Provider value={{ isLoading, setGames, games,getGames, message, setUser, user, setIsLoading, setMessage, setTimedMessage}} >
           {children}
+          
         </LoadingContext.Provider>
+
+        
+
+       </div>
       );
 }
 
--- client-project3/src/pages/Games.js
+++ client-project3/src/pages/Games.js
@@ -1,12 +1,34 @@
+import { useEffect, createContext, useContext, useState } from "react";
+import { LoadingContext } from "../context/loading.context"
 
-const Games = () => {
 
-    return (
-        <div>
-            
 
-        </div>
-    )
+
+const Games = ({}) => {
+
+    const { getGames,setGames,games} = useContext(LoadingContext)
+
+    
+
+    useEffect(() => {
+        getGames()
+}, [])
+
+
+return (
+    <div>
+      <h1>Games</h1>
+      {games ? (
+        games.map((game) => (
+          <div key={game.id}>
+            <p>{game.name}</p>
+          </div>
+        ))
+      ) : (
+        <h4>Loading...</h4>
+      )}
+    </div>
+  );
 }
 
 export default Games
