d7fb5ee49abdf3899e78254cc465dc8738e917a1
Luis Isaac <luiscubiche@gmail.com>
2023-03-06 13:16:14 -0500

    psuhin again

--- client-project3/src/context/loading.context.js
+++ client-project3/src/context/loading.context.js
@@ -11,7 +11,7 @@ const LoadingProvider = ({ children }) => {
     const [isLoading, setIsLoading] = useState(false);
     const [user, setUser] = useState(null);
     const [message, setMessage] = useState('');
-    const [ games, setGames ] = useState();
+    const [ games, setGames ] = useState([]);
     const [ gamesParams, setGamesParams ] = useState(1);
     const [page, setPage] = useState(1); // current page number
     const [page_size, setPageSize] = useState(20); // number of games per page
@@ -26,26 +26,29 @@ const LoadingProvider = ({ children }) => {
 // 43a14242dd124f1fb5e0bb64b4a70da0
 // games?key=<key from RAWG>
 
-const getGames = async () => {
+const getGames = () => {
   
-  if (!games) {
+  
 
   
 	
     console.log("Calling API"); 
-		await axios.get(
+		 axios.get(
 			`https://rawg-video-games-database.p.rapidapi.com/games?key=43a14242dd124f1fb5e0bb64b4a70da0`,
 			{
 				headers: {
           'X-RapidAPI-Key': '3ab9eedeeemsh37ec609bf36b9b6p1b04b2jsnc001e82699f7',
           'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
 				},
-				params: {page:page,page_size:page_size}
+				params: {page:1, page_size:page_size}
 			}
 		)
         .then((response)=>{
-               setGames(response.data.results)
 
+         // let newArr = [...games];
+          //newArr.push(...response.data.results)
+            //   setGames(response.data.results)
+            setGames((prev) => [...prev, ...response.data.results])
                setGamesParams(response.data.next) 
                console.log('page',  page);
                 console.log('res',  response.data);
@@ -58,7 +61,47 @@ const getGames = async () => {
         })
 
 
-};
+
+
+}
+
+
+const getNewGames =  () => {
+  
+  
+
+  
+	
+  console.log("Calling API"); 
+   axios.get(
+    `https://rawg-video-games-database.p.rapidapi.com/games?key=43a14242dd124f1fb5e0bb64b4a70da0`,
+    {
+      headers: {
+        'X-RapidAPI-Key': '3ab9eedeeemsh37ec609bf36b9b6p1b04b2jsnc001e82699f7',
+        'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
+      },
+      params: {page:page,page_size:page_size}
+    }
+  )
+      .then((response)=>{
+
+       // let newArr = [...games];
+        //newArr.push(...response.data.results)
+          //   setGames(response.data.results)
+          setGames((prev) => [...prev, ...response.data.results])
+             setGamesParams(response.data.next) 
+             console.log('page',  page);
+              console.log('res',  response.data);
+           console.log('this is the response', response)
+                
+              
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+
+
 
 }
 
@@ -85,7 +128,7 @@ const getGames = async () => {
 //{fetchQuotes && <Games data={{fetchQuotes}} />}
     return (
       <div>
-        <LoadingContext.Provider value={{ isLoading,page,page_size,setPage, gamesParams,setGamesParams, games,getGames, message, setUser, user, setIsLoading, setMessage, setTimedMessage}} >
+        <LoadingContext.Provider value={{ isLoading, page,page_size,setPage,getNewGames, gamesParams,setGamesParams, games,getGames, message, setUser, user, setIsLoading, setMessage, setTimedMessage}} >
           {children}
           
         </LoadingContext.Provider>
--- client-project3/src/pages/Games.js
+++ client-project3/src/pages/Games.js
@@ -7,79 +7,104 @@ import GameDetail from "./GamesDetail";
 
 const Games = () => {
 
-  const { getGames, setGames,getParams,page,page_size,setPage, games, gamesParams,setGamesParams } = useContext(LoadingContext)
+  const { getGames, setGames, getNewGames, getParams,page,page_size,setPage, games, gamesParams,setGamesParams } = useContext(LoadingContext)
 
 
-  useEffect(() => {
-
-    getGames()
-  }, [page])
+ 
 
 
-  const NextPage = (next) => {
+  const NextPage = () => {
     
     
       setPage(page + 1);
+    // setPage(previousCount => previousCount + 1, getGames())
     
   }
 
- 
+  useEffect(() => {
+    if(!games.length) {
+      getGames()
+    }
+  
+}, [])
+
+useEffect(() => {
+  if(page > 1){
+
+    getNewGames()
+  }
+  
+   
+  
+
+}, [page])
+  
   
   return (
-
     <div className="parentImg">
-
       <br />
-      {games ? (
-        games.map((game) => (
-          <div className="game-card-container" key={game.id}>
-                       
-              <div className="game-card">
-              <Link to={`/games/${game.id}`} > 
-              <img src={game.background_image} alt="Gameimg" />
-              </Link> 
-              <h2>{game.name}</h2>
-              <ul>
-                <li><strong>Genre </strong> {game.genres[0].name}</li>
-                <li><strong>Rating </strong>{game.rating}</li>
-                <li><strong>ESRB Rating </strong>{game.esrb_rating.name}</li>
-                <li><strong>Playtime </strong>{game.playtime} hrs</li>
-                <li><strong>Released: </strong>{game.released}</li>
-              </ul>
-           
-            </div>
+      {games.length && console.log("Games now", games)}
+      {games.length ? (
+        <>
+          {games.map((game) => {
             
-          </div>
+            
+              return (
+                <>
 
-
-
-        ))
-
-      )
-
-
-        : (
-          <h4>Loading...</h4>
-        )}
-
-      {games && games.length > 0 && (
-       <footer>  <button onClick={NextPage}>Next Page</button>  </footer> 
-                
+            {game && 
+                <div className="game-card-container" key={game.id}>
+                  <div className="game-card">
+                    <Link to={`/games/${game.id}`}>
+                      <img src={game.background_image} alt="Gameimg" />
+                    </Link>
+                    <h2>{game.name}</h2>
+                    <ul>
+                      {game.genres.length && (
+                        <li>
+                          <strong>Genre </strong> {game.genres[0].name}
+                        </li>
+                      )}
+                      <li>
+                        <strong>Rating </strong>
+                        {game.rating}
+                      </li>
+                      {game.esrb_rating.name && (
+                        <li>
+                          <strong>ESRB Rating </strong>
+                          {game.esrb_rating.name}
+                        </li>
+                      )}
+                      <li>
+                        <strong>Playtime </strong>
+                        {game.playtime} hrs
+                      </li>
+                      <li>
+                        <strong>Released: </strong>
+                        {game.released}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+          }
+                </>
+              );
+            }
+          )}
+        </>
+      ) : (
+        <h4>Loading...</h4>
       )}
 
-
-
-
-
-
-
+      { games.length > 0 && (
+        <footer>
+          {" "}
+          <button onClick={()=>NextPage()}>Next Page</button>{" "}
+        </footer>
+      )}
     </div>
-
-
-
-
-
   );
+
 }
 
 
