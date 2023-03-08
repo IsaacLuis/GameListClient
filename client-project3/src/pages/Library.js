e09d57dda2ba9d25bd55808699c844ab22e06349
Luis Isaac <luiscubiche@gmail.com>
2023-03-08 17:07:00 -0500

    deploy ready front

--- client-project3/src/pages/Library.js
+++ client-project3/src/pages/Library.js
@@ -2,6 +2,9 @@ import { LoadingContext } from "../context/loading.context";
 import { useState, useEffect } from "react";
 import { useContext} from 'react'
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { baseUrl } from "../services/baseUrl";
+
 const Library = () => {
     const {
         user, 
@@ -17,9 +20,21 @@ const Library = () => {
       
     },[user])
 
+
+    const handleAddToWishlistDelete = (user, game) => {
+      axios.post(`http://localhost:4000/games/delete/add-wish/${user._id}`, { game: game })
+        .then((response) => {
+          console.log('Game deleted', response.data);
+          setUser(response.data)
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+      };
+
     return (
         
-        <div>
+        <div className="parentLib">
             
           {user  ? (
             <>
@@ -53,6 +68,7 @@ const Library = () => {
                       </li>
                     </ul>
                   </div>
+                  <button onClick={() => handleAddToWishlistDelete(user, game)}>Delete</button>
                 </div>
               ))}
           
--- client-project3/src/services/baseUrl.js
+++ client-project3/src/services/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = "http://localhost:4000"
+export const baseUrl = "https://server-project-3.fly.dev"
